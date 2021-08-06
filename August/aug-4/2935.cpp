fea65b697c3716d16b8c56741aa4bf26699b9cc9
geekbgm <bgm1425@gmail.com>
2021-08-07 03:08:47 +0900

    resolving problem

--- August/aug-4/2935.cpp
+++ August/aug-4/2935.cpp
@@ -4,14 +4,47 @@ using namespace std;
 
 int main(){
 
-    long long A,B;
+    string A,B;
+    int count_A=0, count_B=0;
+    int total;
     char opr;
     cin >> A>>opr>>B;
 
-    if(opr == '*'){
-        cout<<A*B<<endl;
+    if (opr == '+'){
+
+        if (A.length() > B.length()){
+
+            string result = A;
+
+            int idx = A.length() - B.length();
+
+            result[idx] = '1';
+
+            cout << result << "\n";
+
+        }else if (A.length() == B.length()){
+
+            string result = A;
+            result[0] = '2';
+            cout << result << "\n";
+
+        }else{
+
+            string result = B;
+            int idx = B.length() - A.length();
+            result[idx] = '1';
+            cout << result << "\n";
+
+        }
+
     }else{
-        cout<<A+B<<endl;
+
+            int len = A.length() - 1 + B.length() - 1;
+            string result = "1";
+            
+            for (int i = 0; i < len; i++) result += '0';
+            
+            cout << result << "\n";
     }
 
 
